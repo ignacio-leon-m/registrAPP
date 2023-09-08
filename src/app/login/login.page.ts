0b7171b219cb46d495d0b87e66110bad581f1684
ignacio-leon-m <ig.leon@duocuc.cl>
2023-09-08 20:16:31 -0300

    segundo commit agregando logica rutas

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+interface Usuario {
+  nombre: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-login',
@@ -7,9 +14,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  formIngresarUsuario: FormGroup; //Declarando formulario reactivo
+  
+  usuario1: Usuario = {
+    nombre: 'Jose',
+    password: '1234'
+  }
+
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.formIngresarUsuario = this.fb.group(
+      {
+        nombre: [""],
+        password: [""]
+      }
+    )
+   }
 
   ngOnInit() {
+  }
+
+  ingresarUsuario(){
+    this.router.navigate(['home']);
   }
 
 }
--- src/app/recupera-pass/recupera-pass.page.ts
+++ src/app/recupera-pass/recupera-pass.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recupera-pass',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecuperaPassPage implements OnInit {
 
-  constructor() { }
+  formularioRecuperacion: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router) { 
+    this.formularioRecuperacion = this.fb.group(
+      {
+        nombre: [""]
+      }
+    )
+  }
 
   ngOnInit() {
+  }
+
+  recuperar() {
+    this.router.navigate(['home']);
   }
 
 }
