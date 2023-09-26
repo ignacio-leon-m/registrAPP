6727749907520b206d91d69d57c3cca7446aa069
ignacio-leon-m <ig.leon@duocuc.cl>
2023-09-26 13:01:02 -0300

    avanzando en el proyeecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LoginGuard } from './login/login.guard';
 
 const routes: Routes = [
   {
@@ -19,6 +20,12 @@ const routes: Routes = [
     path: 'recupera-pass',
     loadChildren: () => import('./recupera-pass/recupera-pass.module').then( m => m.RecuperaPassPageModule)
   },
+  {
+    path: 'private',
+    canActivate: [LoginGuard],
+    loadChildren: () => import('./private/private.module').then( m => m.PrivatePageModule)
+  },
+
 ];
 
 @NgModule({
---
+++ src/app/login/login.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  private isLogged: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+
+  getIsLogged(){
+    return this.isLogged.asObservable();
+  }
+
+  setIsLogged(value: boolean){
+    this.isLogged.next(value);
+  }
+
+  constructor() { }
+}
